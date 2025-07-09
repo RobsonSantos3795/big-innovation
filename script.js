1c215d421d9108803bb83f1344245ef39487442e
Robson G. <123911094+RobsonSantos3795@users.noreply.github.com>
2025-07-09 10:32:12 -0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,21 @@
+var logina = document.getElementById("logina");
+var menu = document.getElementById("menu");
+
+logina.addEventListener("click", function() {
+    menu.classList.toggle("visible");
+    if (menu.style.display == "block") {
+        menu.style.display = "none";
+    } else {
+        menu.style.display = "block";
+    }
+})
+
+function suspender() {
+    let suspenso = document.getElementById("suspenso");
+    let suspiro = suspenso.value;
+    if (suspiro) {
+        window.location.href = suspiro;
+    } else {
+        alert("Erro de codificação!");
+    }
+}
